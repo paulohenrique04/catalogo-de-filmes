d6e8fb3d23480255b6e101137616163143f5fa01
paulohenrique04 <paulohab2004@gmail.com>
2025-10-25 23:39:40 -0300

    Adição de novas configurações para testar o envio de email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { 
     host: 'catalogo-de-filmes-rz92.onrender.com', 
     protocol: 'https'
@@ -63,12 +63,16 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
-    port: 587,
+    port: 2525,
     domain: 'catalogo-de-filmes-rz92.onrender.com', 
     user_name: 'apikey', 
     password: ENV['SENDGRID_API_KEY'],
     authentication: :plain,
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    open_timeout: 10, # Adicione isso
+    read_timeout: 10,
+    ssl: false,
+    tls: true
   }
 
 
