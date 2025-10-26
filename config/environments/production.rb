a54d843cf8a452effa0a74452a982f1507761f62
paulohenrique04 <paulohab2004@gmail.com>
2025-10-25 23:53:13 -0300

    Mudança nas configurações

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { 
     host: 'catalogo-de-filmes-rz92.onrender.com', 
     protocol: 'https'
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
-    port: 2525,
+    port: 587,
     domain: 'catalogo-de-filmes-rz92.onrender.com', 
     user_name: 'apikey', 
     password: ENV['SENDGRID_API_KEY'],
